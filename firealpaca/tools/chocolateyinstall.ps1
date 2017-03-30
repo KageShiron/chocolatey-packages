6d4494cd29e11254cf9c11689dddf5966c4d09fb
KageShiron <kageshiron@gmail.com>
2017-03-31 01:45:01 +0900

    Add update.ps1

--- firealpaca/tools/chocolateyinstall.ps1
+++ firealpaca/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName= 'firealpaca' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '' # download url, HTTPS preferred
+$url        = 'http://firealpaca.com/download/win' # download url, HTTPS preferred
 $url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 #$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
@@ -25,7 +25,7 @@ $packageArgs = @{
   # To determine checksums, you can get that from the original site if provided. 
   # You can also use checksum.exe (choco install checksum) and use it 
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   checksum64    = ''
   checksumType64= 'sha256' #default is checksumType
---
+++ firealpaca/update.ps1
@@ -0,0 +1,39 @@
+﻿$releases = 'http://firealpaca.com/'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $version = ($download_page.AllElements | ? class -eq "count-new").innerText
+    return @{ 
+        Version = $version;
+        URL32 = "http://firealpaca.com/download/win";
+        ChecksumType32 = 'sha256'
+    }
+}
+
+function au_BeforeUpdate() {
+    $options =
+    @{
+        Referer = 'http://firealpaca.com/';
+      
+    }
+
+    Invoke-WebRequest -Headers $options -uri $Latest.URL32 -OutFile $fn -UseBasicParsing
+    $res = Get-FileHash $fn -Algorithm $Algorithm  | % Hash
+    rm $fn -ea ignore
+    return $res.ToLower()
+
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
+            "(?i)(^\s*checksum\s*=\s*)'.*'"   = "`$1'$($Latest.Checksum32)'"      #2
+        }
+    }
+}
+
+
+update -NoCheckUrl -ChecksumFor none
