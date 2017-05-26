e9b462a358fdc92698cf52437d9e9591023e8bf6
KageShiron <kageshiron@gmail.com>
2017-05-26 19:41:23 +0900

    Fix installer

--- pandoc-crossref/update.ps1
+++ pandoc-crossref/update.ps1
@@ -1,18 +1,17 @@
+
+#github API
 $releases = "https://api.github.com/repos/lierdakil/pandoc-crossref/releases"
 
 function global:au_GetLatest {
     $json = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0];
-    $max = New-Object PSObject | select pandocver,ghcver,url
+    $max = New-Object PSObject | select pandocver, ghcver, url
     $max.ghcver = 0
     $max.pandocver = 0
-    foreach( $asset in $json.assets)
-    {
-        if($asset.name -match "windows-ghc(\d.*?)-pandoc(.*?)\.zip")
-        { 
-            $pandocver = $matches[2] -replace "-","."
+    foreach ( $asset in $json.assets) {
+        if ($asset.name -match "windows-ghc(\d.*?)-pandoc(.*?)\.zip") { 
+            $pandocver = $matches[2] -replace "-", "."
             $ghcver = $matches[1]
-            if( $ghcver -ge $max.ghcver -and  $pandocver -ge $max.pandocver)
-            {
+            if ( $ghcver -ge $max.ghcver -and $pandocver -ge $max.pandocver) {
                 $max.ghcver = $ghcver;
                 $max.pandocver = $pandocver;
 
@@ -21,8 +20,8 @@ function global:au_GetLatest {
         }
     }
     return @{ 
-        Version = $json.tag_name -replace "v" , "";
-        URL32 = $max.url
+        Version        = $json.tag_name -replace "v" , "";
+        URL32          = $max.url
         ChecksumType32 = 'sha256'
     }
 }
@@ -30,8 +29,8 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
-            "(?i)(^\s*checksum\s*=\s*)'.*'"   = "`$1'$($Latest.Checksum32)'"      #2
+            "(^[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"           #1
+            "(?i)(^\s*checksum\s*=\s*)'.*'" = "`$1'$($Latest.Checksum32)'"      #2
         }
     }
 }
