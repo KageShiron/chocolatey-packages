eb0e4373597a9ff7c81f6886d9b86741a84ffe49
Chocolatey <chocolatey@realdimensions.net>
2017-08-31 04:09:15 +0000

    AU: 1 updated - favbinedit [skip ci] https://gist.github.com/e7a862a33142f54a2250b657ac2182f6/f362677813660a87d1e5601d1831d4eb783ad3dd

--- favbinedit/tools/chocolateyinstall.ps1
+++ favbinedit/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'favbinedit'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://ftp.vector.co.jp/69/09/3447/FavBinEdit-1-1-8.exe'
+$url        = 'http://ftp.vector.co.jp/69/35/3447/FavBinEdit-1-1-9.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   softwareName  = 'FavBinEdit*'
 
-  checksum      = '2192d05c0df6562e99a60cac4b33ca2a065be938ea8bd7f1510df6aa8f7b5c85'
+  checksum      = '9634c298991bdb261ee2293ae4938c9f1b122746bf219051e803229c6c606a15'
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
