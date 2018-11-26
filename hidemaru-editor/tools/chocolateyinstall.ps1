d25d69dc70541a55f183edfb08a123ff6e2ebf34
Chocolatey <chocolatey@realdimensions.net>
2018-11-26 03:23:39 +0000

    AU: 1 updated - hidemaru-editor [skip ci] https://gist.github.com/e7a862a33142f54a2250b657ac2182f6/75ffc253bb4c4f117a7b0ec71aacff577f53dc59

--- hidemaru-editor/tools/chocolateyinstall.ps1
+++ hidemaru-editor/tools/chocolateyinstall.ps1
@@ -12,19 +12,19 @@ if ($null -eq $hmtype) {
 }
 
 $types = @{
-    'url32'       = 'http://hide.maruo.co.jp/software/bin3/hm886_signed.exe'
-    'url32e'      = 'http://hide.maruo.co.jp/software/bin3/maruo886_signed.exe'
-    'url32f'      = 'http://hide.maruo.co.jp/software/bin3/hmfloat886_signed.exe'
-    'url64'       = 'http://hide.maruo.co.jp/software/bin3/hm886_x64_signed.exe'
-    'url64e'      = 'http://hide.maruo.co.jp/software/bin3/maruo886_x64_signed.exe'
-    'url64f'      = 'http://hide.maruo.co.jp/software/bin3/hmfloat886_x64_signed.exe'
+    'url32'       = 'http://hide.maruo.co.jp/software/bin3/hm887_signed.exe'
+    'url32e'      = 'http://hide.maruo.co.jp/software/bin3/maruo887_signed.exe'
+    'url32f'      = 'http://hide.maruo.co.jp/software/bin3/hmfloat887_signed.exe'
+    'url64'       = 'http://hide.maruo.co.jp/software/bin3/hm887_x64_signed.exe'
+    'url64e'      = 'http://hide.maruo.co.jp/software/bin3/maruo887_x64_signed.exe'
+    'url64f'      = 'http://hide.maruo.co.jp/software/bin3/hmfloat887_x64_signed.exe'
     
-    'checksum32'  = 'be169df7df365113a781d1889afa9d4919c5ec0f92d7022aa04e4143ad3bdd82'
-    'checksum32e' = '3288e7343092b2f11d41ac791e8cca0d0719eca00f5903ef9de6ce4d18096fd4'
-    'checksum32f' = 'a102c6d2cf57eb7add9fb231c0be64cff3ac70051eb558e662082e07e8ebabb9'
-    'checksum64'  = '0367593dc56f5abf532cd76e877c92a8cd4f516c096b2909fc7f37b41992b63f'
-    'checksum64e' = '5a2505e562506abc658da9f40fb7b9d9a7106ede281308a32f15f5d12e38f8cb'
-    'checksum64f' = '3ba193e1e2098af48931d2114c09011ed4aa7d4861c275abb897748b3e410a31'
+    'checksum32'  = '9ecbda1d85fc5b35554d906d3bc7362b1deacb7dca3eda69aa580d27be8e5a8b'
+    'checksum32e' = 'a5a422e89a35c7151d430729efd60612c5d801cab5f87fbe79a5d844942c031b'
+    'checksum32f' = '4a09847c5a443fdcd5194e1b41da0e33a7caef4227722af234d717bed4d2d665'
+    'checksum64'  = '6fcd8673b1609ca497af88ea729a06119be87b377ae0b885ff105aac0b5857b9'
+    'checksum64e' = 'd9b36b3f13703c60c54b6323346446500677fae233822f34e01a75f5fdcecc38'
+    'checksum64f' = '8f9c8438d813d24ca4a020f537ef0116a153d27b15364c0b14e2a7be1984a416'
 }
 
 $url = $types["url" + $hmtype]
