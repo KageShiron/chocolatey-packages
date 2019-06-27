4248f6e4f1a5eaacd6c8f09d83ee2531bd467fe4
Chocolatey <chocolatey@realdimensions.net>
2019-06-27 03:20:58 +0000

    AU: 1 updated - usamimi-hurricane [skip ci] https://gist.github.com/e7a862a33142f54a2250b657ac2182f6/367ac59c7efc6601c2616373516a39863af533c0

--- usamimi-hurricane/tools/chocolateyinstall.ps1
+++ usamimi-hurricane/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'usamimi-hurricane'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://ftp.vector.co.jp/70/90/2117/UsaMimi_v029.zip'
+$url        = 'http://ftp.vector.co.jp/71/76/2117/UsaMimi_v030.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'zip'
   url           = $url
   softwareName  = 'UsaMimi Hurricane*'
-  checksum      = '4df541d4ce3b1152f24e92548a01f6d372f539f71ccb791c5b8749d73a5286ce'
+  checksum      = '5379c6d9536c6c1ff08d1d4955468e6bb4b6949918ac2a6d84e7550b76190f9b'
   checksumType  = 'sha256'
 
 }
